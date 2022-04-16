8d9fe5e12f697b8352c73ae2249fa6d122064eea
e-kalmar <e.kalmar94@gmail.com>
2022-04-16 08:00:22 +0300

    MDS-js update

--- js/scripts.js
+++ js/scripts.js
@@ -66,7 +66,7 @@
     // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
       target: '#mainNav',
-      offset: 70
+      offset: 71
     });
   
     // Collapse Navbar
